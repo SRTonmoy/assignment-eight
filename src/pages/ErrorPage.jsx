ee39c39b9fd81ce76e2c7f4bcadaaf432a2d8c74
SRTonmoy <sahriarrahman701@gmail.com>
2025-10-08 14:55:26 +0600

    added the error page uodate

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import err from '../assets/error-404.png'
 
-
 export default function ErrorPage(){
-return (
-<div className="container" style={{textAlign:'center',paddingTop:48}}>
-<img src={err} alt="404" style={{maxWidth:400,width:'100%'}} />
-<h2>Page Not Found</h2>
-<p>Sorry, we couldn't find the page you're looking for.</p>
-<Link to="/" className="btn-primary">Go Home</Link>
-</div>
-)
+  return (
+    <div className="error-page-container">
+      <div className="error-content">
+        <img src={err} alt="404 - Page Not Found" className="error-image" />
+        <h2>Page Not Found</h2>
+        <p>Sorry, we couldn't find the page you're looking for.</p>
+        <Link to="/" className="btn-primary">Go Home</Link>
+      </div>
+    </div>
+  )
 }
